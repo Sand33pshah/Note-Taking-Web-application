11baef9301b990edb3ab5ab4c85687f285808245
Sand33pshah <88148562+Sand33pshah@users.noreply.github.com>
2022-02-19 17:50:17 +0530

    Add files via upload

---
+++ website/static/index.js
@@ -0,0 +1,7 @@
+function deleteNote(noteID) {
+    fetch("/delete-note", {
+        method: "POST", 
+        body: JSON.stringify({ noteID: noteID }),}).then((_res) => {
+            window.location.href = "/";
+        });
+}
